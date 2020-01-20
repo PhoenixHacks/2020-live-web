7e1fdf43df757d7335240f7ce52d7ae4fe42e389
Justin McGettigan <jwmcgettigan@gmail.com>
2020-01-20 03:22:16 -0500

    Transitioning from css to scss

--- src/Components/Media.js
+++ src/Components/Media.js
@@ -5,17 +5,25 @@ import { FaWifi, FaDiscord, FaUpload, FaImages } from 'react-icons/fa';
 
 const MediaLinks = () => {
   return (
-    <div className="media-links col-6">
-      <div className="card d-flex align-items-start justify-content-center" style={{height: "130px"}}>
-        <h5><FaDiscord/>
-          <a href="https://phoenixhacks.com/discord" className="" target="_blank" rel="noopener noreferrer">
-          Discord</a></h5>
-        <h5><img style={{marginRight: "-3px"}} src={require("../images/devpost-icon3.png")}/>
-          <a href="https://phoenixhacks2020.devpost.com" className="" target="_blank" rel="noopener noreferrer">
-          DevPost</a></h5>
-        <h5><FaImages/>
-          <a href="https://phoenixhacks.com/media" className="" target="_blank" rel="noopener noreferrer">
-          Media</a></h5>
+    <div className="media-links col-7">
+      <div className="card d-flex justify-content-center row" style={{height: "130px"}}>
+        <div className="col">
+          <h5><FaDiscord/>
+            <a href="https://phoenixhacks.com/discord" className="" target="_blank" rel="noopener noreferrer">
+            Discord</a></h5>
+          <h5><img style={{marginRight: "-3px"}} src={require("../images/devpost-icon3.png")}/>
+            <a href="https://phoenixhacks2020.devpost.com" className="" target="_blank" rel="noopener noreferrer">
+            DevPost</a></h5>
+          <h5><FaImages/>
+            <a href="https://phoenixhacks.com/media" className="" target="_blank" rel="noopener noreferrer">
+            Media</a></h5>
+        </div>
+
+        <div className="col">
+          <h5 className="row"><i class="fa fa-question-circle" aria-hidden="true"></i>
+            <a href="https://phoenixhacks.com#FAQ" className="" target="_blank" rel="noopener noreferrer">
+            FAQ</a></h5>
+        </div>
       </div>
     </div>
   )
@@ -23,7 +31,7 @@ const MediaLinks = () => {
 
 const Wifi = () => {
   return (
-    <div className="wifi col-6">
+    <div className="wifi col-5">
       <div className="card justify-content-center">
         <h3 className="wifi-title"><i class="fa fa-wifi"></i> WiFi</h3>
         <h5>Poly_Guest</h5>
