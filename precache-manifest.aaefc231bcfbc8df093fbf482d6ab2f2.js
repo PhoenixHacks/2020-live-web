b2bcf6a32001409c566a86dc2201fc0ffcfc2fb7
Justin McGettigan <jwmcgettigan@gmail.com>
2020-01-21 07:25:33 -0500

    Updates

---
+++ precache-manifest.aaefc231bcfbc8df093fbf482d6ab2f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6567de03cf85b9c1374188d45de8f32",
+    "url": "/2020-live-web/index.html"
+  },
+  {
+    "revision": "13802b5668d619bb61a6",
+    "url": "/2020-live-web/static/css/main.17390d35.chunk.css"
+  },
+  {
+    "revision": "bd414dbf3ee000103faf",
+    "url": "/2020-live-web/static/js/2.ab508233.chunk.js"
+  },
+  {
+    "revision": "28dad01e7c8e7601dbe29b8c05486bb4",
+    "url": "/2020-live-web/static/js/2.ab508233.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13802b5668d619bb61a6",
+    "url": "/2020-live-web/static/js/main.d9c159ae.chunk.js"
+  },
+  {
+    "revision": "3866778ebaf3603d094d",
+    "url": "/2020-live-web/static/js/runtime-main.c7a58986.js"
+  },
+  {
+    "revision": "b3483ec650bba4a4416d39c4d6030a99",
+    "url": "/2020-live-web/static/media/BAHNSCHRIFT.b3483ec6.TTF"
+  },
+  {
+    "revision": "542e62bb09ad2e2c952a35d815cf1b29",
+    "url": "/2020-live-web/static/media/IST.542e62bb.png"
+  },
+  {
+    "revision": "52c92d6c54334d411570e1307b302684",
+    "url": "/2020-live-web/static/media/PhoenixHacks-Regular.52c92d6c.ttf"
+  }
+]);
