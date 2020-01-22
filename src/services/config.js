41feabeba3ca587b0197f8499379367b391f5719
Justin McGettigan <jwmcgettigan@gmail.com>
2020-01-22 01:46:19 -0500

    Updated API URL

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 //const config = { API_URL: "https://api.phoenixhacks.com" };
 //const config = { API_URL: "https://api-2020.now.sh:3001" };
-const config = { API_URL: "https://localhost:3001" };
+const config = { API_URL: "https://phoenixhacks-live.herokuapp.com/" };
 
 export default config;
